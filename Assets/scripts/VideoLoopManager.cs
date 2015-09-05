24dc74027bb22d5097333e4b5a83e78b3587ee72
Cecilia Lejeune <cecilia.lejeu@gmail.com>
2015-09-05 16:48:44 +0200

    * Associate animation with each state in the animator * Get the current state in the VideoLoopManager

---
+++ Assets/scripts/VideoLoopManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VideoLoopManager : MonoBehaviour {
+	
+	private Animator _loopAnimatior;
+	private StateEnum _currentState;
+
+	void Awake () {
+		//Retrieve the Game object
+		_loopAnimatior = GameObject.Find ("LoopAnim").GetComponent<Animator>();
+		_currentState = StateEnum.none;
+	}
+
+
+	void Update () {
+		AnimatorStateInfo state = _loopAnimatior.GetCurrentAnimatorStateInfo (0);
+		if (state.shortNameHash == Animator.StringToHash (StateEnum.patafoin.ToString ())) {
+			_currentState = StateEnum.patafoin;
+			Debug.Log ("patafoin");
+		} else if (state.shortNameHash == Animator.StringToHash (StateEnum.plop.ToString ())) {
+			_currentState = StateEnum.plop;
+			Debug.Log ("plop");
+		} else if (state.shortNameHash == Animator.StringToHash (StateEnum.pif.ToString ())) {
+			_currentState = StateEnum.pif;
+			Debug.Log ("pif");
+		} else if (state.shortNameHash == Animator.StringToHash (StateEnum.patapon.ToString ())) {
+			_currentState = StateEnum.patapon;
+			Debug.Log ("patapon");
+		} else if (state.shortNameHash == Animator.StringToHash (StateEnum.pouf.ToString ())) {
+			_currentState = StateEnum.pouf;
+			Debug.Log ("pouf");
+		} else {
+			_currentState = StateEnum.none;
+		}
+	}
+
+}
